3d26ced87b038eb9aed107ea470d55c0331747b0
Maria <maria.carranzavaldes@gmail.com>
2022-06-13 09:14:18 +0200

    rutas anidadas

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {BrowserRouter as Router, Link, 
+
+  Routes, Route} from 'react-router-dom'
+
+import './App.css';
+
+import Home from './Pages/Home';
+
+import Courses from './Pages/Courses';
+
+import Search from './components/Search';
+
+import List from  './components/Search';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Router>
+
+        <nav>
+
+          <Link to="/">Home</Link>
+
+          <Link to="courses">Courses</Link>
+
+        </nav>
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="/courses" element={<Courses />}>
+
+            <Route path="search" element={<Search />} />
+
+            <Route path="list" element={<List/>}/>
+
+          </Route>
+
+        </Routes>
+
+     </Router>
+
+    </div>
+
+  );
+
+}
+
+ 
+
+export default App;
